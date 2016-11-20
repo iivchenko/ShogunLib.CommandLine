46eb8c2d9ef85526a58d4fafbee9d9b86ab618ad
shogun <iivchenko@live.com>
2016-11-20 18:51:07 +0200

    Move samples to a new location
    Issue #3

---
+++ Src/ShogunLib.CommandLine.Samples/OwnClassesImplementation/OwnExitCommand.cs
@@ -0,0 +1,49 @@
+﻿// <copyright company="XATA">
+//      Copyright (c) 2012, All Right Reserved
+// </copyright>
+// <author>Ivan Ivchenko</author>
+// <email>iivchenko@live.com</email>
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using ShogunLib.CommandLine.Commands;
+using ShogunLib.CommandLine.Commands.Parameters;
+
+namespace OwnClassesImplementation
+{
+    public class OwnExitCommand : ICommand
+    {
+        private readonly ExitMonitor _exitMonitor;
+
+        public OwnExitCommand(ExitMonitor exitMonitor)
+        {
+            _exitMonitor = exitMonitor;
+        }
+
+        public string Name
+        {
+            get { return "OwnExitCommand"; }
+        }
+
+        public string Description
+        {
+            get { return "This command do something useful"; }
+        }
+
+        public IEnumerable<IParameterInfo> Parameters
+        {
+            get { return new List<IParameterInfo>(); }
+        }
+
+        public void Execute(IEnumerable<string> args)
+        {
+            Console.WriteLine("Exiting");
+            Thread.Sleep(5000);
+            Console.WriteLine("Exited");
+            Thread.Sleep(5000);
+
+            _exitMonitor.IsContinue = false;
+        }
+    }
+}
---
+++ Src/ShogunLib.CommandLine.Samples/OwnClassesImplementation/OwnInterpreter.cs
@@ -0,0 +1,37 @@
+﻿// <copyright company="XATA">
+//      Copyright (c) 2012, All Right Reserved
+// </copyright>
+// <author>Ivan Ivchenko</author>
+// <email>iivchenko@live.com</email>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ShogunLib.CommandLine.Commands;
+using ShogunLib.CommandLine.Interpretation;
+using ShogunLib.CommandLine.Interpretation.Parsing;
+
+namespace OwnClassesImplementation
+{
+    public class OwnInterpreter : IInterpreter
+    {
+        private const string HelpCommandName = "Help";
+        private readonly IInterpreter _interpreter;
+        
+        public OwnInterpreter(IEnumerable<ICommand> commands)
+        {
+            _interpreter = new Interpreter(new InputParser(), commands.ToDictionary(command => command.Name.ToUpperInvariant()), HelpCommandName);
+        }
+
+        public event EventHandler<HelpCommandEventArgs> Help
+        {
+            add { _interpreter.Help += value; }
+            remove { _interpreter.Help -= value; }
+        }
+        
+        public void Execute(string input)
+        {
+            _interpreter.Execute(input);
+        }
+    }
+}
